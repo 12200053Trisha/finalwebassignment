fbcdaac7cd26ca1917e22f0ee33a9c6ff336bf7e
12200053Trisha <141597169+12200053Trisha@users.noreply.github.com>
2023-09-11 10:36:20 +1000

    changes 1

---
+++ group - Copy/group/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace group.Models
+{
+    public class Class
+    {
+       
+ public int ClassID { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+
+        public virtual ICollection<Membership> Memberships { get; set; }
+       // public virtual ICollection<Session> Session { get; set; }
+
+
+    }
+}
---
+++ group - Copy/group/Models/Membership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace group.Models
+{
+    public class Membership
+    {
+        public int MembershipID { get; set; }
+        public int MemberID { get; set; }
+        public int ClassID { get; set; }
+        public string MembershipType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public virtual Member Member { get; set; }
+        public virtual Class Class { get; set; }
+
+        //public virtual Session Session {get; set; }
+
+
+        
+    }
+}
